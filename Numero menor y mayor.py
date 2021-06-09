6ea8f8a00908cd865a36288d86ec9006b7335709
Luigui024 <L4_098@hotmail.com>
2021-06-09 17:48:35 -0500

    git add .

---
+++ Numero menor y mayor.py
@@ -0,0 +1,19 @@
+#Numero Menor y Mayor
+cantidadNumeros = int(input('\nDigite la cantidad de números: '))
+numeros=[]
+print('')
+for i in range(cantidadNumeros):
+    numeros.append(int(input('Digite el '+str(i+1)+'° número: ')))
+
+numeroMayor = numeros[0]
+numeroMenor = numeros[0]
+
+for i in numeros:
+
+    if(numeroMayor<i):
+        numeroMayor=i
+
+    if(numeroMenor>i):
+        numeroMenor=i
+
+print('\nEl número menor es:',numeroMenor,'\nEl número mayor es:',numeroMayor,'\n')
